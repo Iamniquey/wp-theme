eb7ed31d22419e26f8c0cefe62f44d5d7775a825
Iamniquey <iamnique@gmail.com>
2025-12-13 13:30:21 -0500

    add title

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <?php
     while(have_posts()) {
         the_post(); ?>
-        hello
+        <div>
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        </div>
     <?php }
 
 ?>
